8d5723bc6ab6ff5f062ea3814895dd75acd8f69b
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2020-09-01 15:45:10 +0700

    add

---
+++ car.js
@@ -0,0 +1,15 @@
+const X_CAR = 390;
+const Y_CAR = 760;
+let ctx = document.getElementById("myCanvas").getContext("2d");
+
+class Car{
+    constructor() {
+        this.x = X_CAR;
+        this.y = Y_CAR;
+    }
+    drawCar(){
+        ctx.beginPath();
+
+    }
+
+}
